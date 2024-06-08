5db6d78d21260cee69705bcc4268e907b7f61ab3
My Name <rsalomon230@gmail.com>
2024-06-08 18:59:53 +0300

    final to submit

--- Post.py
+++ Post.py
@@ -1,24 +1,98 @@
 from datetime import datetime
+import re
+import json
 
 
-# this class get all the post data and use it to display as we want
 class Post:
-    def __init__(self, title, author, publish_datetime, text):
-        self.title = title
-        self.author = author
-        self.publish_datetime = self.convert_date(publish_datetime)
-        self.text = text
+    # we define patterns to use for each url.
+    patterns2 = {
+        "post_pattern": r'<div class="b-post__grid-container">(.*?)<!-- /end .b-post -->',
+        "title_pattern": r'<h2.*?>(.*?)</h2>',
+        "author_pattern": r'<span itemprop="name">(.*?)</span>',
+        "datetime_pattern": r'<time(.*?)>(.*?)</time>',
+        "text_pattern": r'<div.*? itemprop="text">\s*(.*?)\s*(<div class="h-flex-spacer'
+                        r' h-margin-top-16">|<div class="post-signature restore">)',
+        "ID": "2"
+    }
+    patterns1 = {
+        "post_pattern": r'<div class="postbody">(.*?)</div>(?!<)',
+        "title_pattern": r'<h3.*?>\s*<a.*?>(.*?)</a>',
+        "author_pattern": r'class="username.*?">(.*?)</a>',
+        "datetime_pattern": r'<time(.*?)>(.*?)</time>',
+        "text_pattern": r'<div class="content">(.*)',
+        "ID": "1"
+    }
+    # we insert th url with the matching patterns to a dictionary
+    URL_dictionary = {'https://forum.vbulletin.com/forum/vbulletin-3-8/vbulletin-3-8-questions-problems-and-'
+                      'troubleshooting/414325-www-vs-non-www-url-causing-site-not-to-login': patterns2,
+                      'http://www.phpbb.com/community/viewtopic.php?f=46&t=2159437': patterns1}
 
-    def to_dict(self):
-        return {
-            "title": self.title,
-            "text": self.text,
-            "published": self.publish_datetime,
-            "author": self.author
-        }
+    def __init__(self, url):
+        self.url_check(url)
+        self.url = url
+
+    @staticmethod
+    def url_check(url):
+        if url not in Post.URL_dictionary.keys():
+            raise Exception("This is not a valid URL")
 
     # this function receive data for date and time . parse it and return the format we want
-    def convert_date(self, date):
+    def scrape_posts(self, html_content):
+        posts_list = []
+        # Find all matches of the post pattern
+        posts = re.findall(self.URL_dictionary[self.url]["post_pattern"], html_content, re.DOTALL)
+
+        for post_content in posts:
+            # Extract post metadata
+            title_match = re.search(self.URL_dictionary[self.url]["title_pattern"], post_content, re.DOTALL)
+            author_match = re.search(self.URL_dictionary[self.url]["author_pattern"], post_content)
+
+            datetime_match = re.search(self.URL_dictionary[self.url]["datetime_pattern"], post_content, re.DOTALL)
+            text_match = re.search(self.URL_dictionary[self.url]["text_pattern"], post_content, re.DOTALL)
+            # Get matched groups
+            title = title_match.group(1).strip() if title_match else None
+            author = author_match.group(1).strip() if author_match else None
+            publish_datetime = datetime_match.group(2) if datetime_match else None
+            text = text_match.group(1)
+            # Replace <br> with newline
+            text = text.replace('<br>', '\n')
+
+            # Remove all other HTML tags
+            # text = re.sub(r'</div>', '', text).strip()
+            text = re.sub(r'<.*?>', '', text).strip()
+            text = text.replace('\n', '')
+            text = text.replace('\t', '')
+            text = text.replace('\r', '')
+            text = text.replace('&quot;', '"')
+            text = re.sub(r'<a(.*?)>', ':', text).strip()
+            text = re.sub(r'<.*?>', '\n', text).strip()
+
+            # Create Post instance
+            post_info = self.to_dict(title, author, publish_datetime, text)
+            posts_list.append(post_info)
+        name = "Post" + self.URL_dictionary[self.url]["ID"] + ".json"
+        self.save_posts_to_json(posts_list, name)
+        print("succeded! the file " + name + " has been updated ")
+
+    @staticmethod
+    def save_posts_to_json(posts, filename):
+        json_list = []
+        for post in posts:
+            json_list.append(post)
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(json_list, file, ensure_ascii=False, indent=4)
+
+    @staticmethod
+    def to_dict(title, text, publish_datetime, author):
+        return {
+            "title": title,
+            "text": text,
+            "published": publish_datetime,
+            "author": author
+        }
+
+    @staticmethod
+    def convert_date(date):
         date_formats = [
             "%a %d %b '%y, %I:%M%p",
             "%a %b %d, %Y %I:%M %p"
--- main.py
+++ main.py
@@ -1,114 +1,37 @@
-import re
-import json
 import sys
 import requests
 from Post import Post
 
-# we define patterns to use for each url.
 
-patterns2 = {
-    "post_pattern": r'<div class="b-post__grid-container">(.*?)<!-- /end .b-post -->',
-    "title_pattern": r'<h2.*?>(.*?)</h2>',
-    "author_pattern": r'<span itemprop="name">(.*?)</span>',
-    "datetime_pattern": r'<time(.*?)>(.*?)</time>',
-    "text_pattern": r'<div.*? itemprop="text">\s*(.*?)\s*(<div class="h-flex-spacer'
-                    r' h-margin-top-16">|<div class="post-signature restore">)',
-    "ID": "2"
-}
+def main():
+    # receive arguments
+    arg = sys.argv
+    if len(arg) <= 1:
+        raise Exception("not valid number of arguments")
 
-patterns1 = {
-    "post_pattern": r'<div class="postbody">(.*?)</div>(?!<)',
-    "title_pattern": r'<h3.*?>\s*<a.*?>(.*?)</a>',
-    "author_pattern": r'class="username.*?">(.*?)</a>',
-    "datetime_pattern": r'<time(.*?)>(.*?)</time>',
-    "text_pattern": r'<div class="content">(.*)',
-    "ID": "1"
-}
+    p = Post(arg[1])
 
-# we insert th url with the matching patterns to a dictionary
-URL_dictionary = {'https://forum.vbulletin.com/forum/vbulletin-3-8/vbulletin-3-8-questions-problems-and-'
-                  'troubleshooting/414325-www-vs-non-www-url-causing-site-not-to-login': patterns2,
-                  'http://www.phpbb.com/community/viewtopic.php?f=46&t=2159437': patterns1}
+    inp = "p"
+    while inp != "e":
+        # get the html response
+        try:
+            response = requests.get(p.url)
 
-# receive arguments
-arguments = sys.argv
-if len(arguments) > len(URL_dictionary) or len(arguments) <= 1:
-    raise Exception("not valid number of arguments")
-
-url = arguments[1]
-
-if url not in URL_dictionary.keys():
-    raise Exception("This is not a valid URL")
-
-regex = URL_dictionary[url]
+            if response.status_code == 200:
+                # Scrape posts from the HTML we got
+                p.scrape_posts(response.text)
+                inp = "e"
 
 
-# we save th pots to a json file
-def save_posts_to_json(posts, filename):
-    json_list = []
-    for post in posts:
-        json_list.append(post.to_dict())
+        except requests.RequestException as e:
 
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(json_list, file, ensure_ascii=False, indent=4)
+            print(f"An error occurred: {e}")
 
+            retry = input("Do you want to try again? (y/n): ")
 
-# we start scraping the HTML
-def scrape_posts(html_content):
-    posts_list = []
-    # Find all matches of the post pattern
-    posts = re.findall(URL_dictionary[url]["post_pattern"], html_content, re.DOTALL)
-
-    for post_content in posts:
-        # Extract post metadata
-        title_match = re.search(URL_dictionary[url]["title_pattern"], post_content, re.DOTALL)
-        author_match = re.search(URL_dictionary[url]["author_pattern"], post_content)
-
-        datetime_match = re.search(URL_dictionary[url]["datetime_pattern"], post_content, re.DOTALL)
-        text_match = re.search(URL_dictionary[url]["text_pattern"], post_content, re.DOTALL)
-        # Get matched groups
-        title = title_match.group(1).strip() if title_match else None
-        author = author_match.group(1).strip() if author_match else None
-        publish_datetime = datetime_match.group(2) if datetime_match else None
-        text = text_match.group(1)
-        # Replace <br> with newline
-        text = text.replace('<br>', '\n')
-
-        # Remove all other HTML tags
-        # text = re.sub(r'</div>', '', text).strip()
-        text = re.sub(r'<.*?>', '', text).strip()
-        text = text.replace('\n', '')
-        text = text.replace('\t', '')
-        text = text.replace('\r', '')
-        text = text.replace('&quot;', '"')
-        text = re.sub(r'<a(.*?)>', ':', text).strip()
-        text = re.sub(r'<.*?>', '\n', text).strip()
-
-        # Create Post instance
-        post = Post(title, author, publish_datetime, text)
-        posts_list.append(post)
-    name = "Post" + URL_dictionary[url]["ID"] + ".json"
-    save_posts_to_json(posts_list, name)
-    print("succeded! the file " + name + " has been updated ")
+            if retry.lower() != 'y':
+                inp = "e"  # Exit the loop if the user doesn't want to retry
 
 
-inp = "p"
-while inp != "e":
-    # get the html response
-    try:
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            # Scrape posts from the HTML we got
-            scrape_posts(response.text)
-            inp = "e"
-
-
-    except requests.RequestException as e:
-
-        print(f"An error occurred: {e}")
-
-        retry = input("Do you want to try again? (y/n): ")
-
-        if retry.lower() != 'y':
-            inp = "e"  # Exit the loop if the user doesn't want to retry
+if __name__ == "__main__":
+    main()
